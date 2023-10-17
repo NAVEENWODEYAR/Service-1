7a8363d7cfbd45c1e0e6ace90cfb057c68d070c5
Naveen <naveenkrishnawodeyar@gmail.com>
2023-10-17 21:49:17 +0530

    Citizen entity defined

---
+++ src/main/java/com/micros/modal/Citizen.java
@@ -0,0 +1,29 @@
+package com.micros.modal;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "Citizen_Table")
+public class Citizen 
+{
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int cID;
+	
+	@Column(name = "Citizen_Name")
+	private String cName;
+	
+	@Column(name = "Center_ID")
+	private int vaccinationCenterId;
+}

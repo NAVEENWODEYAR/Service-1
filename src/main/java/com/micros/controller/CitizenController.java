86e0ccc96492364fd65c1bf6e16c6f125467895a
Naveen <naveenkrishnawodeyar@gmail.com>
2023-10-17 22:19:07 +0530

    swagger ui added

--- src/main/java/com/micros/controller/CitizenController.java
+++ src/main/java/com/micros/controller/CitizenController.java
@@ -1,11 +1,35 @@
 package com.micros.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.micros.modal.Citizen;
+import com.micros.service.CitizenService;
 
 @RestController
 @RequestMapping("/citizen")
 public class CitizenController 
 {
+	@Autowired
+	private CitizenService citizenService;
 	
+	// 1. get data,
+	@GetMapping("/getCitizen")
+	private List<Citizen> getCitizens()
+	{
+		List<Citizen> list = citizenService.getCitizens();
+		return list;
+	}
+	
+	// 2. send data.,
+	@PostMapping("/addCitizen")
+	private Citizen insertCitizen(Citizen citizen)
+	{
+		return citizenService.addCitizen(citizen);
+	}
 }

710db209c311c62f1b993119c7697af15a9b55af
Naveen <naveenkrishnawodeyar@gmail.com>
2023-10-19 11:42:07 +0530

    @LoadBalanced

--- src/main/java/com/micros/Service1Application.java
+++ src/main/java/com/micros/Service1Application.java
@@ -2,6 +2,9 @@ package com.micros;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class Service1Application 
@@ -11,6 +14,13 @@ public class Service1Application
 	{
 		SpringApplication.run(Service1Application.class, args);
 		System.out.println("\n Module 1 started.,");
+	}
+	
+	@Bean
+	@LoadBalanced
+	public RestTemplate getTemplate()
+	{
+		return new RestTemplate();
 	}
 
 }

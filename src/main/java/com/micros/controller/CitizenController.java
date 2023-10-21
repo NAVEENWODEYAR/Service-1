44afc8058ef8dd569f503cf83040fb6ecb2853e6
Naveen <naveenkrishnawodeyar@gmail.com>
2023-10-21 14:15:35 +0530

    import org.springframework.web.bind.annotation.RestController;

--- src/main/java/com/micros/controller/CitizenController.java
+++ src/main/java/com/micros/controller/CitizenController.java
@@ -3,7 +3,7 @@ package com.micros.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;

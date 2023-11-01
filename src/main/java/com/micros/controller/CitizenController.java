9cc3b0a93b8405c30564564608d40bc46757f9f6
Naveen <naveenkrishnawodeyar@gmail.com>
2023-11-01 21:31:36 +0530

    import java.util.*;

--- src/main/java/com/micros/controller/CitizenController.java
+++ src/main/java/com/micros/controller/CitizenController.java
@@ -1,6 +1,6 @@
 package com.micros.controller;
 
-import java.util.List;
+import java.util.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;

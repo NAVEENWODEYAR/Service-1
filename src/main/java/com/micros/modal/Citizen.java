07f56149c6e0366a00725f5ac3abc66ac1545f92
Naveen <naveenkrishnawodeyar@gmail.com>
2023-11-13 22:30:55 +0530

    import jakarta.persistence.*;

--- src/main/java/com/micros/modal/Citizen.java
+++ src/main/java/com/micros/modal/Citizen.java
@@ -1,6 +1,6 @@
 package com.micros.modal;
 
-import jakarta.persistence.Column;
+import jakarta.persistence.*;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
